8345c1dbcbd3e5bc764ea746b5a6473fc25f08ac
JayasriRamu <jvembu74@gmail.com>
2023-01-30 22:15:19 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Todo from './Todo';
+
+import App from './App';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <Todo/>
+    <App />
+  </StrictMode>
+);
